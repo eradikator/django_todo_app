1f743df02991ebcfa2b4e4195c7e092811d2fb77
eradikator <stehlik.michael@gmail.com>
2021-06-19 22:08:39 +0200

    Vytvořen login, logout, register

--- todo/forms.py
+++ todo/forms.py
@@ -1,6 +1,14 @@
-from django import forms 
+from django import forms
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TodoForm(forms.Form):
     text = forms.CharField(max_length=40, 
         widget=forms.TextInput(
             attrs={'class' : 'form-control', 'placeholder' : 'Enter todo e.g. Delete junk files', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
